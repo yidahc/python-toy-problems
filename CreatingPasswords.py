83307f96401ee17f6836f6b3234c48ba6e7cca28
yidahc <46684324+yidahc@users.noreply.github.com>
2019-08-22 19:37:05 -0500

    added placeholders and typecasting to solution, with notes

--- CreatingPasswords.py
+++ CreatingPasswords.py
@@ -53,3 +53,27 @@ print ('Second password: ' + pswd2)
 
 print('\nNumber of characters in',pswd1+':',len(pswd1))
 print('Number of characters in',pswd2+':',len(pswd2))
+
+
+
+# Redoing toy problem with string formatting
+
+favoriteColor = input('Enter favorite color: \n')
+petsName = input('Enter pet\'s name: \n')
+num = int(input('Enter a number: \n'))
+# converting to int, since that is the data type we want to work with
+
+print ('\n%s %s %d' % (favoriteColor, petsName, num))
+
+pswd1 = favoriteColor+'_'+petsName
+print ('\nFirst password: %s_%s' % (favoriteColor, petsName))
+
+# %s and %d serve as placeholders for a string and digit respectively, so we can write the whole sentece without concatinating
+# variables to replace placeholders are all writen afterwards, after the %, for easier legibility
+
+pswd2 = str(num)+favoriteColor+str(num)
+#converting to string, as python does not automatically typecast
+print ('Second password: %d%s%d' % (num, favoriteColor, num))
+
+print('\nNumber of characters in %s: %d' % (pswd1,len(pswd1)))
+print('Number of characters in %s: %d' % (pswd2,len(pswd2)))
