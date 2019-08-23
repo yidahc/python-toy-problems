fba387affdf83a90332bcdab3b4a6420b417349e
yidahc <46684324+yidahc@users.noreply.github.com>
2019-08-22 23:36:45 -0500

    finished HelloWorld

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+""" Write a program that outputs the following, ending the program's output with a newline:
+
+Hello world!
+How are you?
+   (I'm fine).
+    """
+
+print("Hello world!\n" "How are you?\n" "   (I'm fine).\n")
