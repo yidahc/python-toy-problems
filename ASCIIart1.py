ff6c351b4b065406b2e5434c806590fbfd5c6420
yidahc <46684324+yidahc@users.noreply.github.com>
2019-08-22 23:52:55 -0500

    finished ASCIart1

---
+++ ASCIIart1.py
@@ -0,0 +1,24 @@
+""" (1) Output this tree.
+   *
+  ***
+ *****
+*******
+  ***
+
+(2) Below the tree (with two blank lines), output this cat.
+/\   /\
+  o o
+ =   =
+  ---
+Hint: A backslash \ in a string acts as an escape character, such as with a newline \n. So, to print an actual backslash, escape that backslash by prepending another backslash. Ex: The following prints a single backslash: print('\\') """
+
+print('   *')
+print('  ***')
+print(' *****')
+print('*******')
+print('  ***\n\n')
+
+print('/\\   /\\')
+print('  o o')
+print(' =   =')
+print('  ---')
