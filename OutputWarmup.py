e6511ac65e88419a3ff026870870c2a8613cf8d2
yidahc <46684324+yidahc@users.noreply.github.com>
2019-08-22 23:42:06 -0500

    finished step 1 of OutputWarmup

---
+++ OutputWarmup.py
@@ -0,0 +1,24 @@
+""" 
+A variable like userNum can store a value like an integer. Extend the given program to print userNum values as indicated.
+(1) Output the user's input. 
+Enter integer: 4
+You entered: 4
+
+(2) Extend to output the input squared and cubed. Hint: Compute squared as userNum * userNum. 
+Enter integer: 4
+You entered: 4
+4 squared is 16 
+And 4 cubed is 64 !! 
+
+(3) Extend to get a second user input into userNum2. Output sum and product.
+Enter integer: 4
+You entered: 4
+4 squared is 16 
+And 4 cubed is 64 !!
+Enter another integer: 5
+4 + 5 is 9
+4 * 5 is 20 """
+
+userNum = int(input('Enter integer: '))
+
+print('You entered:', userNum)
